3149854b43c173bdb7a19226ee19cde5bcc081d2
Bhumika Deshmukh <bhumika@gmail.com>
2026-07-30 07:49:43 +0000

    Initial commit: Flask application setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Cloud Native DevSecOps CI/CD Platform</h1>
+    <h2>Welcome to My Flask Application</h2>
+    <p>Created by Bhumika Deshmukh</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
